91bcff9b578b57576d7019257a10d074ff48ce93
ritabugking <ritabugking@gmail.com>
2016-10-18 13:40:42 -0700

    Assignment 2

---
+++ Deque.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author yijuwang
+ * @param <Item>
+ */
+public class Deque<Item> implements Iterable<Item> {
+    
+   private Node first, last;
+   private int n;
+   
+   public Deque() {
+   
+       first = null;
+       last = null;
+       n = 0;
+       
+   }                           // construct an empty deque
+   
+   private class Node {
+   
+       Item item;
+       Node next;
+       Node pre;
+   
+   }
+           
+   public boolean isEmpty() {
+   
+       return first == null;
+   }                // is the deque empty?
+   public int size() {
+       return n;
+   }                       // return the number of items on the deque
+   public void addFirst(Item item) {
+       
+       if (item == null) throw new NullPointerException("item is null");
+       n++;
+       Node oldfirst = first;
+       first = new Node();
+       first.item = item;
+       
+       if (isEmpty())
+           last = first;
+       else {
+           first.next = oldfirst;
+           oldfirst.pre = first;
+       }
+       
+           
+   }         // add the item to the front
+   public void addLast(Item item) {
+   
+       if (item == null) throw new NullPointerException("item is null");
+       n++;
+       Node oldlast = last;
+       last = new Node();
+       last.item = item;
+       
+       if (isEmpty())
+           first = last;
+       else {
+           oldlast.next = last;
+           last.pre = oldlast;
+       }
+   
+   }          // add the item to the end
+   public Item removeFirst() {
+   
+       if (isEmpty()) throw new NoSuchElementException("No item can be poped out");
+       n--;
+       Item item = first.item;
+       first = first.next;
+       if (isEmpty()) {
+           last = null;
+       }
+       else 
+           first.pre = null;
+       return item;
+   }               // remove and return the item from the front
+   public Item removeLast() {
+   
+       if (isEmpty()) throw new NoSuchElementException("No item can be poped out");
+       n--;
+       Item item = last.item;
+       if (first == last) {
+       
+           first = null;
+           last = null;
+       }
+       else {
+           last = last.pre;
+           last.next = null;
+           
+       }
+       
+       return item;
+       
+   }                // remove and return the item from the end
+ 
+   
+   @Override
+   public Iterator<Item> iterator() {
+   
+       return new DequeIterator();
+       
+   }        // return an iterator over items in order from front to end
+   private class DequeIterator implements Iterator<Item> {
+   
+       private Node current = first;
+       
+       @Override
+       public boolean hasNext() {
+           return current != null;
+       }
+       
+       @Override
+       public Item next() {
+       
+           if (current == null) {
+               throw new NoSuchElementException("No item can be poped out");
+           }
+           Item item = current.item;
+           current =current.next;
+           
+           return item;
+       }
+       @Override
+       public void remove() {
+           throw new UnsupportedOperationException("UnsupportedOperationException");
+       }
+       
+   }
+   
+   
+   
+   public static void main(String[] args) {
+   
+   }   // unit testing
+}
+
---
+++ RandomizedQueue.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import edu.princeton.cs.algs4.StdRandom;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author yijuwang
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+   
+    private Node first, last;
+    private int size;
+    
+    public RandomizedQueue() {
+   
+        first = null;
+        last = null;
+        size = 0;
+   
+   }                // construct an empty randomized queue
+    private class Node {
+        
+        Item item;
+        Node next;
+
+    }
+   public boolean isEmpty() {
+       
+       return first == null;
+   }                // is the queue empty?
+   public int size() {
+   
+       return size;
+   }                 // return the number of items on the queue
+   public void enqueue(Item item) {
+       if (item == null) throw new NullPointerException("item is null");
+       Node oldlast = last;
+       last = new Node();
+       last.item = item;
+       if (isEmpty())
+           first = last;
+       else
+           oldlast.next = last;
+       
+       size++;
+       
+       
+   }          // add the item
+   public Item dequeue() {
+   
+       if (size == 0) throw new NoSuchElementException("out of bounds");
+       Item item;
+       if (size == 1) {
+           item = first.item;
+           first = last = null;
+           size--;
+           return item;
+       }
+       Node current = first;
+       int random = (int) StdRandom.uniform() * size + 1;
+       if (random == 1) {
+       
+           item = first.item;
+           first = first.next;
+           size--;
+           return item;
+       }
+       
+       for (int i = 0; i <random-2; i++) {
+       
+           current = current.next;
+           
+       }
+       item = current.next.item;
+       current.next = current.next.next;
+       
+       size--;
+       return item;
+       
+   }                   // remove and return a random item
+   public Item sample() {
+   
+       if (size == 0) throw new NoSuchElementException("no item in queue");
+       Item item;
+       Node current = first;
+       int random = (int) StdRandom.uniform() * size + 1;
+       if (random == 1) {
+           item = first.item;
+           return item;
+       }
+       for (int i = 0; i < random-2; i++) {
+           current = current.next;
+       }
+       item = current.next.item;
+       return item;
+   }                    // return (but do not remove) a random item
+   public Iterator<Item> iterator() {
+   
+       return new RandomizedQueueIterator();
+       
+   }        // return an iterator over items in order from front to end
+   private class RandomizedQueueIterator implements Iterator<Item> {
+   
+       private Node current = first;
+       
+       @Override
+       public boolean hasNext() {
+           return current != null;
+       }
+       
+       @Override
+       public Item next() {
+       
+           if (current == null) {
+               throw new NoSuchElementException("No item can be poped out");
+           }
+           Item item = current.item;
+           current =current.next;
+           
+           return item;
+       }
+       @Override
+       public void remove() {
+           throw new UnsupportedOperationException("UnsupportedOperationException");
+       }
+       
+   }        // return an independent iterator over items in random order
+   public static void main(String[] args) {}  // unit testing
+}
